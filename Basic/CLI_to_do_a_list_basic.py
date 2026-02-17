f9d4b126616e0eb507a0bcd89a2b354a8abf4b25
Shahd Almasri <sh.409137619@gmail.com>
2026-02-17 16:18:17 +0200

    Update CLI_to_do_a_list_basic.py

--- Basic/CLI_to_do_a_list_basic.py
+++ Basic/CLI_to_do_a_list_basic.py
@@ -1,77 +1,79 @@
-import ManageTask_basic
+import manage_task_basic
 
+def main():
+    auto_id=0
+    tasks = []
 
+    print("\n👋 Welcome to our system")
+    while True:
+        print("------------------- Menu -------------------")
+        print("Choose what you want to do:")
+        print("""
+                1. Add Task
+                2. List Tasks
+                3. Update status Task
+                4. Remove Task
+                5. Remove all tasks
+                6. Exit
+        """)
 
-auto_id=0
-tasks = []
+        try:
+            choice = int(input("Enter your choice: "))
 
-print("\n👋 Welcome to our system")
-while True:
-    print("------------------- Menu -------------------")
-    print("Choose what you want to do:")
-    print("""
-            1. Add Task
-            2. List Tasks
-            3. Update status Task
-            4. Remove Task
-            5. Remove all tasks
-            6. Exit
-    """)
+            match choice:
+                case 1:
+                    print("Enter the new task information: ")
+                    task = {}
+                    task["id"] = auto_id
+                    auto_id += 1
 
-    try:
-        choice = int(input("Enter your choice: "))
+                    task["name"] = input("Enter task name: ")
+                    task["description"] = input("Enter description: ")
+                    task["status"] = manage_task_basic.Status.PENDING.value
+                    tasks.append(task)
 
-        match choice:
-            case 1:
-                print("Enter the new task information: ")
-                task = {}
-                task["id"] = auto_id
-                auto_id += 1
+                case 2:
+                    print("\n------- All Tasks -------")
+                    manage_task_basic.print_all_task(tasks)
 
-                task["name"] = input("Enter task name: ")
-                task["description"] = input("Enter description: ")
-                task["status"] = ManageTask_basic.Status.PENDING.value
-                tasks.append(task)
-
-            case 2:
-                print("\n------- All Tasks -------")
-                ManageTask_basic.print_all_task(tasks)
-
-            case 3:
-                print("\n------- All Tasks -------")
-                ManageTask_basic.print_all_task(tasks)
-                id=input("Enter the id of task that you want to update: ")
-                new_status= input("""
-                Choose 1 or 2:
-                1. Pending
-                2. Completed
-                """)
-                if new_status not in ["1", "2"]:
-                    print("Invalid choice ❌")
-                else:
-                    if (new_status == "1"):
-                        pc=1
+                case 3:
+                    print("\n------- All Tasks -------")
+                    manage_task_basic.print_all_task(tasks)
+                    task_id=input("Enter the id of task that you want to update: ")
+                    new_status= input("""
+                    Choose 1 or 2:
+                    1. Pending
+                    2. Completed
+                    """)
+                    if new_status not in ["1", "2"]:
+                        print("Invalid choice ❌")
                     else:
-                        pc=2
-                    ManageTask_basic.update_status(tasks, id, pc)
+                        if (new_status == "1"):
+                            pc=1
+                        else:
+                            pc=2
+                        manage_task_basic.update_status(tasks, task_id, pc)
 
-            case 4:
-                 id = input("Enter the id of task that you want to delete: ")
-                 ManageTask_basic.delete_task(tasks, id)
+                case 4:
+                     task_id = input("Enter the id of task that you want to delete: ")
+                     manage_task_basic.delete_task(tasks, task_id)
 
-            case 5:
-                id = input("Are you sure you want to delete this task? (y/n): ")
-                if id == "y":
-                 ManageTask_basic.remove_all_task(tasks)
-                else:
-                    pass
+                case 5:
+                    task_id = input("Are you sure you want to delete this task? (y/n): ")
+                    if task_id == "y":
+                     manage_task_basic.remove_all_task(tasks)
+                    else:
+                        pass
 
-            case 6:
-                print("\nGoodbye👋")
-                break
+                case 6:
+                    print("\nGoodbye👋")
+                    break
 
-            case _:
-                print("Invalid choice ❌")
+                case _:
+                    print("Invalid choice ❌")
 
-    except:
-        print("Enter a numeric value")
+        except:
+            print("Enter a numeric value")
+
+if __name__ == "__main__":
+    main()
