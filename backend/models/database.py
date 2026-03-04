d618e89ea17b3768eb387694c87e5c78c0cecb3f
Shahd Almasri <sh.409137619@gmail.com>
2026-03-04 16:21:28 +0200

    Update database.py

--- backend/models/database.py
+++ backend/models/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
