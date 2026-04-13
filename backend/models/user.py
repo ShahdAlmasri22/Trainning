229d7223fa7f206b0d1ae0d1acf3e0277cda7d28
Shahd Almasri <sh.409137619@gmail.com>
2026-04-13 14:18:51 +0300

    Update user.py

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from enum import Enum
-
 from sqlalchemy import Column, Integer, String, DateTime, Enum as SqlEnum
 from sqlalchemy.orm import relationship
 from .database import Base
